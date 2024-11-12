c135ea3842313bdc1665bca89866259b5f7690ca
Mainagithubrit <njorogefrancismaina@gmail.com>
2024-11-12 15:26:59 +0300

    Task 3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""A class that manages an API authentication"""
+
+from flask import request
+import os
+from typing import List, TypeVar
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """A function that returns false"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """A function that returns None"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """A function that returns None"""
+        return None
+
+
+if __name__ == '__main__':
+    a = Auth()
