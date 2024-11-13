1a122a913f42ccf20eb7e5c78fa3d04e034091d5
Mainagithubrit <njorogefrancismaina@gmail.com>
2024-11-13 20:06:36 +0300

    Task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -22,7 +22,8 @@ class BasicAuth(Auth):
 
     def decode_base64_authorization_header(
         self,
-        base64_authorization_header: str) -> str:
+        base64_authorization_header: str,
+    ) -> str:
         """This returns the decoded value of a Base64 string"""
         if base64_authorization_header is None:
             return None
