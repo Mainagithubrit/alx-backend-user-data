517a11c86f23859aa7d2adfc7283592f1cee9185
Mainagithubrit <njorogefrancismaina@gmail.com>
2024-11-21 23:56:28 +0300

    Task 6

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""A function that creates a Flask app"""
+
+from flask import Flask, jsonify, Response
+
+app = Flask(__name__)
+
+@app.route("/")
+def home() -> Response:
+    """ Returns To Home"""
+    message = {"message": "Bienvenue"}
+    return jsonify(message)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
