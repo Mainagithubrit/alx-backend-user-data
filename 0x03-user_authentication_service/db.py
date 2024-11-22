b173299969714d9d0920818e5db506d6634bc839
Mainagithubrit <njorogefrancismaina@gmail.com>
2024-11-22 22:21:13 +0300

    Updated

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """DB module
 """
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, tuple_
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
 
 from user import Base, User
 
@@ -43,13 +42,19 @@ class DB:
 
     def find_user_by(self, **kwargs) -> User:
         """Finds a user in a database using keyword arguments"""
-        for key in kwargs:
-            if not hasattr(User, key):
-                raise InvalidRequestError(f"Invalid field: {key}")
-        user = self._session.query(User).filter_by(**kwargs).first()
-        if user is None:
-            raise NoResultFound()
-        return user
+        fields, values = [], []
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                fields.append(getattr(User, key))
+                values.append(value)
+            else:
+                raise InvalidRequestError()
+            result = self._session.query(User).filter(
+                tuple_(*fields).in_([tuple(values)])
+            ).first()
+            if result is None:
+                raise NoResultFound()
+            return result
 
     def update_user(self, user_id: int, **kwargs) -> None:
         """updates a user in a database by using user id as key"""
